d570f7b37ce561a4c507a706bbf3f9059cd21b56
jkjnmlk <123@example.com>
2019-01-16 16:21:39 +0800

    基本框架结构

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
 
-    printf("hello world!");
+    printf("进入敏感字符管理系统\n");
+
+    while (1)
+    {
+
+        printf("1--添加一个敏感字符\n");
+        printf("2--删除最后一个敏感字符\n");
+        printf("3--显示所有敏感字符\n");
+        printf("4--替换所有敏感字符\n");
+        printf("5--退出系统\n");
+        printf("请输入所需项目符号\n");
+        int code;
+        scanf("%d", &code);
+        
+        if (code==1) {
+            /* code */
+        }
+        
+        if (code==2) {
+            /* code */
+        }
+        
+        if (code==3) {
+            /* code */
+        }
+        
+        if (code==4) {
+            /* code */
+        }
+        
+        if (code==5) {
+            /* code */
+        }
+        
+    }
 
     return 0;
-
 }
